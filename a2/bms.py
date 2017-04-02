64c875319027001d608deda67e963f8c634ac7f5
Rahul Bangar <bangar1991@gmail.com>
2017-04-01 18:48:59 -0600

    Reformatting of bms.py

--- a2/bms.py
+++ a2/bms.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python
+
 import random
 import cv2
 import numpy as np
@@ -13,26 +14,22 @@ class BMS:
         self.dilation_width = dilation_width
         self.normalize = normalize
         self.handle_border = handle_border
-        
+
         self.feature_maps = None
         self.mean_attention_map = None
 
-        
     def get_mean_attention_map(self, frame):
 
         height, width = frame.shape[:2]
-        
+
         self.mean_attention_map = np.zeros((height, width), np.float64)
-        
+
         frame_lab = cv2.cvtColor(frame, cv2.COLOR_RGB2LAB)
         self.feature_maps = cv2.split(frame_lab)
 
         self._generate_mean_attention_map(self.sample_step_size)
 
         return self.mean_attention_map
-        
-
-        
 
     def _generate_mean_attention_map(self, step):
         """Generates a mean attention map
@@ -51,20 +48,18 @@ class BMS:
                 # Assign 1 to a pixel if greater than threshold otherwise 0
                 bm = cv2.threshold(f, thresh, 1, cv2.THRESH_BINARY)[1]
                 bm_opened = self._apply_open(bm)
-                self.mean_attention_map +=  self._get_attention_map(bm_opened)
+                self.mean_attention_map += self._get_attention_map(bm_opened)
                 # Generate inverse boolean map
                 bm = cv2.bitwise_not(bm)
                 bm_opened = self._apply_open(bm)
                 self.mean_attention_map += self._get_attention_map(bm_opened)
 
-
-                
     def _apply_open(self, bm):
         """Applies an opening operation on the raw boolean map
 
         Args:
             bm: A boolean map
-        
+
         Returns:
             If opening width > 0, then a new boolean map with opening operation applied, else the same boolean map as input (not a copy)
         """
@@ -76,17 +71,15 @@ class BMS:
         else:
             return bm_opened
 
-
-
     def _get_attention_map(self, bm):
         am = bm.copy()
-        h, w = am.shape[:2] 
+        h, w = am.shape[:2]
 
         # Mask out all the pixels connected to borders
         # since we care only about the pixels that are surrounded
         # and 1 filled regions that are connected to border are
         # not surrounded under the definition
-        
+
         if self.handle_border:
             for row in range(h):
                 # All zero mask means no boundaries for flood filling
@@ -95,46 +88,45 @@ class BMS:
                 # which is 1 by default
                 mask = np.zeros((h+2, w+2), dtype=np.uint8)
                 # Random jump of pixel between 5 and 24 included with probability 0.01, otherwise 0
-                jump = random.randint(5, 24) if random.random()>0.99 else 0
+                jump = random.randint(5, 24) if random.random() > 0.99 else 0
                 if am[row, 0+jump] != 1:
-                    # Flood fill 
-                    cv2.floodFill(am, mask, (0+jump, row), (1,), (0,), (0,), 8) 
+                    # Flood fill
+                    cv2.floodFill(am, mask, (0+jump, row), (1,), (0,), (0,), 8)
                 mask = np.zeros((h+2, w+2), dtype=np.uint8)
                 # Calculate jump from the other end of the row
-                jump = random.randint(5, 24) if random.random()>0.99 else 0
+                jump = random.randint(5, 24) if random.random() > 0.99 else 0
                 if am[row, w-1-jump] != 1:
                     cv2.floodFill(am, mask, (w-1-jump, row), (1,), (0,), (0,), 8)
 
             for col in range(w):
                 mask = np.zeros((h+2, w+2), dtype=np.uint8)
-                jump = random.randint(5, 24) if random.random()>0.99 else 0
+                jump = random.randint(5, 24) if random.random() > 0.99 else 0
                 if am[0+jump, col] != 1:
-                    cv2.floodFill(am, mask, (col, 0+jump), (1,), (0,), (0,), 8) 
+                    cv2.floodFill(am, mask, (col, 0+jump), (1,), (0,), (0,), 8)
                 mask = np.zeros((h+2, w+2), dtype=np.uint8)
-                jump = random.randint(5, 24) if random.random()>0.99 else 0
+                jump = random.randint(5, 24) if random.random() > 0.99 else 0
                 if am[h-1-jump, col] != 1:
                     cv2.floodFill(am, mask, (col, h-1-jump), (1,), (0,), (0,), 8)
-                    
+
         else:
             for row in range(h):
                 mask = np.zeros((h+2, w+2), dtype=np.uint8)
-		if am[row, 0] != 1 :
+                if am[row, 0] != 1:
                     cv2.floodFill(am, mask, (0, row), (1,), (0,), (0,), 8)
                 mask = np.zeros((h+2, w+2), dtype=np.uint8)
                 if am[row, w-1] != 1:
-		    cv2.floodFill(am, mask, (w-1, row), (1,), (0,), (0,), 8)
-	    for col in range(w):
+                    cv2.floodFill(am, mask, (w-1, row), (1,), (0,), (0,), 8)
+                for col in range(w):
+                    mask = np.zeros((h+2, w+2), dtype=np.uint8)
+                if am[0, col] != 1:
+                    cv2.floodFill(am, mask, (col, 0), (1,), (0,), (0,), 8)
                 mask = np.zeros((h+2, w+2), dtype=np.uint8)
-		if am[0, col] != 1:
-		    cv2.floodFill(am, mask, (col,0), (1,), (0,), (0,), 8)
-                mask = np.zeros((h+2, w+2), dtype=np.uint8)
-		if am[h-1, col] != 1:
-		    cv2.floodFill(am, mask, (col,h-1), (1,), (0,), (0,), 8)
+                if am[h-1, col] != 1:
+                    cv2.floodFill(am, mask, (col, h-1), (1,), (0,), (0,), 8)
 
         # Make the pixels not equal to 1 white
         am = (am != 1).astype(np.uint8) * 255
 
-        
         if self.dilation_width > 0:
             am = cv2.dilate(am, None, am, (-1, -1), self.dilation_width)
 
@@ -146,4 +138,3 @@ class BMS:
             cv2.normalize(am, am, 1.0, 0.0, cv2.NORM_MINMAX)
 
         return am
-
